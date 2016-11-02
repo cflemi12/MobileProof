a56e9b9ccad73d78e049c3372e77b31f509843fe
Chase Fleming <cflemi12@jhu.edu>
2016-11-02 09:34:34 -0400

    otherstuff

--- Request.py
+++ Request.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
-from bs4 import BeautifulSoup
 import urllib2
+import sqlite3
 
-# Simple Request that sends the contents of Pythagoras's Theorem from proof
-# wiki
+# Simple Request that sends the contents of Pythagoras's Theorem from SQLite
 def do_request(req):
-    url = 'https://www.proofwiki.org/wiki/Pythagoras\'s_Theorem'
-    req = urllib2.Request(url)
-    response = urllib2.urlopen(req)
-    the_page = response.read()
-    soup = BeautifulSoup(str(the_page), 'html.parser')
-    soup.prettify()
-    return str(soup.find(id="content"))
+    # Connect to Database that already exists
+    conn = sqlite3.connect("/home/chase/MobileProof/database/websites.sqlite")
+    conn.text_factory = str
+    c = conn.cursor()
+    # Search database for Pythagoras's Theorem
+    c.execute("SELECT html from table_of_websites where titles=?",("Pythagoras's Theorem",))
+    # Send Response and close
+    response = c.fetchone()
+    conn.close()
+    return response[0]
 
